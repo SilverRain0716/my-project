7d8440ab08f7d9f5a4d47a899d984916cf0f258b
richspeaker <101845989+richspeaker@users.noreply.github.com>
2023-02-09 21:42:14 +0900

    check

--- Stock_analysis/MyPractics/list.py
+++ Stock_analysis/MyPractics/list.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup as BS
 
-list_csv = pd.read_csv('GITHUB/my-project/Stock_analysis/list_co.csv')
-list_df = pd.DataFrame(list_csv)
-list_df = list_df.sort_values(by='종목코드')
-
-print(list_df[0:])
+list = pd.read_csv('Stock_analysis/MyPractics/list_co.csv')
+list.종목코드 = list.종목코드.map('{06d}'.format)
+print(list)
