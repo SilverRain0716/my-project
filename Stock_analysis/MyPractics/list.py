61cf7ad55983cfd2664e833147b8d91c1990e213
richspeaker <101845989+richspeaker@users.noreply.github.com>
2023-02-13 22:35:04 +0900

    stock

--- Stock_analysis/MyPractics/list.py
+++ Stock_analysis/MyPractics/list.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup as BS
 
-list = pd.read_csv('Stock_analysis/MyPractics/list_co.csv')
-list.종목코드 = list.종목코드.map('{06d}'.format)
-print(list)
+df = pd.read_csv('Stock_analysis/MyPractics/list_co.csv')
+df['종목코드'] = df['종목코드'].map(lambda x: str(x).zfill(6))
+print(df)
