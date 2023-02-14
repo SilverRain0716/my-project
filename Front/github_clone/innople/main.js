4913300a99608eff270da602dc870787f3ffda3f
richspeaker <101845989+richspeaker@users.noreply.github.com>
2023-02-15 02:03:20 +0900

    rolling

--- Front/github_clone/innople/main.js
+++ Front/github_clone/innople/main.js
@@ -1,20 +1,32 @@
 const slider = document.querySelector('.slider');
 const sliderContainer = document.querySelector('.slider-container');
 const sliderItems = document.querySelectorAll('.slider-item');
+const slideCount = sliderItems.length;
+const slideWidth = slider.clientWidth;
+const slideMargin = 10; // Changed slide margin to 10 pixels
 
-let firstItemWidth = 0;
-let currentPosition = 0;
-let newItem;
+sliderContainer.style.width = slideCount * (slideWidth + slideMargin) + "px";
 
-for (let i = 0; i < sliderItems.length; i++) {
-  if (i === 0) {
-    firstItemWidth = sliderItems[i].offsetWidth;
+sliderItems.forEach((item) => {
+  item.style.width = slideWidth + "px";
+});
+
+// Clone the first slider item and append it to the end of the slider
+const firstSlideClone = sliderItems[0].cloneNode(true);
+sliderContainer.appendChild(firstSlideClone);
+
+let currentSlide = 0;
+function transitionSlide() {
+  const slideOffset = currentSlide * (slideWidth + slideMargin);
+  sliderContainer.style.transform = `translateX(-${slideOffset}px)`;
+  
+  currentSlide++;
+  if (currentSlide >= slideCount) {
+    // Reset to the first slide
+    currentSlide = 0;
+    sliderContainer.style.transform = `translateX(0)`;
   }
-  newItem = sliderItems[i].cloneNode(true);
-  sliderContainer.appendChild(newItem);
 }
 
-sliderContainer.style.width = `${sliderItems.length * firstItemWidth}px`;
-
-setInterval(() => {
-  currentPosition -=
+var slideInterval = 4000;
+setInterval(transitionSlide, slideInterval);
